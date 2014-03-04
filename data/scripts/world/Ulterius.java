0a6201c0abf0f1541a27c3c1ffcf826008c53000
Nate_NBJ <Nate_NBJ@localhost>
2014-03-03 17:29:53 -0700

    Weapons added to Idoneus Citadel

--- data/scripts/world/Ulterius.java
+++ data/scripts/world/Ulterius.java
@@ -14,11 +14,50 @@ import com.fs.starfarer.api.campaign.CargoAPI.CrewXPLevel;
 import com.fs.starfarer.api.campaign.FactionAPI;
 import com.fs.starfarer.api.fleet.FleetMemberType;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import org.apache.log4j.Level;
 
 public class Ulterius {
+    public static final Map VENDOR_SHIPS = new HashMap();
+    public static final Map VENDOR_WINGS = new HashMap();
+    public static final Map VENDOR_WEAPONS = new HashMap();
+    public static final Map VENDOR_CREW = new HashMap();
+    public static final int VENDOR_SUPPLIES = 2000;
+    public static final int VENDOR_FUEL = 3000;
+    public static final int VENDOR_MARINES = 100;
 
+    static {
+        VENDOR_CREW.put(CrewXPLevel.GREEN, 500);
+        VENDOR_CREW.put(CrewXPLevel.REGULAR, 1000);
+        VENDOR_CREW.put(CrewXPLevel.VETERAN, 100);
+        VENDOR_CREW.put(CrewXPLevel.ELITE, 25);
+
+        VENDOR_SHIPS.put("sun_ice_voidreaver_Standard", 1);
+        VENDOR_SHIPS.put("sun_ice_kelpie_Standard", 2);
+        VENDOR_SHIPS.put("sun_ice_eidolon_Standard", 2);
+        VENDOR_SHIPS.put("sun_ice_soulbane_Standard", 2);
+        VENDOR_SHIPS.put("sun_ice_nightseer_Standard", 1);
+        VENDOR_SHIPS.put("sun_ice_athame_Standard", 2);
+        VENDOR_SHIPS.put("sun_ice_specter_Standard", 3);
+        VENDOR_SHIPS.put("sun_ice_flashghast_Standard", 3);
+        VENDOR_SHIPS.put("sun_ice_pentagram_Standard", 2);
+
+        VENDOR_WINGS.put("sun_ice_poltergeist_wing", 3);
+        VENDOR_WINGS.put("sun_ice_phantom_wing", 2);
+
+        //VENDOR_WEAPONS.put("sun_ice_hypermassdriver", 2);
+        VENDOR_WEAPONS.put("sun_ice_mobiusray", 8);
+        VENDOR_WEAPONS.put("sun_ice_tractorbeam", 4);
+        VENDOR_WEAPONS.put("sun_ice_boomerangpod", 12);
+        VENDOR_WEAPONS.put("sun_ice_hypermassbomb", 3);
+        VENDOR_WEAPONS.put("sun_ice_flamebolt", 8);
+        VENDOR_WEAPONS.put("sun_ice_hexac", 16);
+        VENDOR_WEAPONS.put("sun_ice_boomerang", 20);
+        VENDOR_WEAPONS.put("sun_ice_scatterpd", 18);
+        VENDOR_WEAPONS.put("sun_ice_lighthexac", 24);
+    }
 	public void generate() {
 		SectorAPI sector = Global.getSector();
 		StarSystemAPI system = sector.createStarSystem("Ulterius");
@@ -66,46 +105,40 @@ public class Ulterius {
 	
 	private void initStationCargo(SectorEntityToken station) {
 		CargoAPI cargo = station.getCargo();
-		addRandomWeapons(cargo, 5);
 		
-		cargo.addCrew(CrewXPLevel.VETERAN, 200);
-		cargo.addCrew(CrewXPLevel.REGULAR, 2000);
-		cargo.addMarines(200);
-		cargo.addSupplies(3000);
-		cargo.addFuel(2000);
+		cargo.addMarines(VENDOR_MARINES);
+		cargo.addSupplies(VENDOR_SUPPLIES);
+		cargo.addFuel(VENDOR_FUEL);
+        
+		cargo.addCrew(CrewXPLevel.GREEN, (Integer)VENDOR_CREW.get(CrewXPLevel.GREEN));
+		cargo.addCrew(CrewXPLevel.REGULAR, (Integer)VENDOR_CREW.get(CrewXPLevel.REGULAR));
+		cargo.addCrew(CrewXPLevel.VETERAN, (Integer)VENDOR_CREW.get(CrewXPLevel.VETERAN));
+		cargo.addCrew(CrewXPLevel.ELITE, (Integer)VENDOR_CREW.get(CrewXPLevel.ELITE));
 
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_voidreaver_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_voidreaver_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_kelpie_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_kelpie_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_eidolon_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_eidolon_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_soulbane_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_soulbane_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_soulbane_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_specter_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_specter_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_specter_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_flashghast_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_flashghast_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_athame_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_athame_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_nightseer_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_nightseer_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_pentagram_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "sun_ice_pentagram_Standard"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.FIGHTER_WING, "sun_ice_poltergeist_wing"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.FIGHTER_WING, "sun_ice_poltergeist_wing"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.FIGHTER_WING, "sun_ice_phantom_wing"));
-		cargo.getMothballedShips().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.FIGHTER_WING, "sun_ice_phantom_wing"));
-	}
-	
-	private void addRandomWeapons(CargoAPI cargo, int count) {
-		List weaponIds = Global.getSector().getAllWeaponIds();
-		for (int i = 0; i < count; i++) {
-			String weaponId = (String) weaponIds.get((int) (weaponIds.size() * Math.random()));
-			int quantity = (int)(Math.random() * 4f + 2f);
-			cargo.addWeapons(weaponId, quantity);
-		}
+        for(Iterator iter = VENDOR_SHIPS.keySet().iterator(); iter.hasNext();) {
+            String id = (String)iter.next();
+            int count = (Integer)VENDOR_SHIPS.get(id);
+            
+            for(int i = 0; i < count; ++i) {
+                cargo.getMothballedShips().addFleetMember(Global.getFactory()
+                        .createFleetMember(FleetMemberType.SHIP, id));
+            }
+        }
+
+        for(Iterator iter = VENDOR_WINGS.keySet().iterator(); iter.hasNext();) {
+            String id = (String)iter.next();
+            int count = (Integer)VENDOR_WINGS.get(id);
+            
+            for(int i = 0; i < count; ++i) {
+                cargo.getMothballedShips().addFleetMember(Global.getFactory()
+                        .createFleetMember(FleetMemberType.FIGHTER_WING, id));
+            }
+        }
+
+        for(Iterator iter = VENDOR_WEAPONS.keySet().iterator(); iter.hasNext();) {
+            String id = (String)iter.next();
+
+            cargo.addWeapons(id, (Integer)VENDOR_WEAPONS.get(id));
+        }
 	}
 }
